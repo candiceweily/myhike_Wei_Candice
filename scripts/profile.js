7985af76a69a219c8fb907ecf2775647fd1dc3f1
Candice Wei <weily00@bu.edu>
2024-03-19 09:39:25 -0700

    Update profile.html and profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -32,3 +32,28 @@ function populateUserInfo() {
 
 //call the function to run it
 populateUserInfo();
+
+function editUserInfo() {
+  //Enable the form fields
+  document.getElementById("personalInfoFields").disabled = false;
+}
+
+function saveUserInfo() {
+  //enter code here
+  //a) get user entered values
+  userName = document.getElementById("nameInput").value; //get the value of the field with id="nameInput"
+  userSchool = document.getElementById("schoolInput").value; //get the value of the field with id="schoolInput"
+  userCity = document.getElementById("cityInput").value; //get the value of the field with id="cityInput"
+  //b) update user's document in Firestore
+  currentUser
+    .update({
+      name: userName,
+      school: userSchool,
+      city: userCity,
+    })
+    .then(() => {
+      console.log("Document successfully updated!");
+    });
+  //c) disable edit
+  document.getElementById("personalInfoFields").disabled = true;
+}
