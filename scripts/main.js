7753a59b76f042b5e398f94fa5bfd9ce91f1eeb7
Candice Wei <weily00@bu.edu>
2024-03-26 08:59:36 -0700

    Finish demo 11 part 1, 2.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,38 @@
-// function getNameFromAuth() {
-//   firebase.auth().onAuthStateChanged((user) => {
-//     // Check if a user is signed in:
-//     if (user) {
-//       // Do something for the currently logged-in user here:
-//       console.log(user.uid); //print the uid in the browser console
-//       console.log(user.displayName); //print the user name in the browser console
-//       userName = user.displayName;
+//Global variable pointing to the current user's Firestore document
+var currentUser;
 
-//       //method #1:  insert with JS
-//       document.getElementById("name-goes-here").innerText = userName;
+//Function that calls everything needed for the main page
+function doAll() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser = db.collection("users").doc(user.uid); //global
+      console.log(currentUser);
 
-//       //method #2:  insert using jquery
-//       //$("#name-goes-here").text(userName); //using jquery
+      // figure out what day of the week it is today
+      const weekday = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+      ];
+      const d = new Date();
+      let day = weekday[d.getDay()];
 
-//       //method #3:  insert using querySelector
-//       //document.querySelector("#name-goes-here").innerText = userName
-//     } else {
-//       // No user is signed in.
-//       console.log("No user is logged in");
-//     }
-//   });
-// }
-// getNameFromAuth(); //run the function
+      // the following functions are always called when someone is logged in
+      readQuote(day);
+      insertNameFromFirestore();
+      displayCardsDynamically("hikes");
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+      window.location.href = "login.html";
+    }
+  });
+}
+doAll();
 
 function insertNameFromFirestore() {
   // Check if the user is logged in:
@@ -42,8 +53,6 @@ function insertNameFromFirestore() {
   });
 }
 
-insertNameFromFirestore();
-
 // Function to read the quote of the day from the Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
 function readQuote(day) {
@@ -61,7 +70,6 @@ function readQuote(day) {
       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
     });
 }
-readQuote("tuesday"); //calling the function
 
 function writeHikes() {
   //define a variable for the collection you want to create in Firestore to populate data
@@ -133,10 +141,21 @@ function displayCardsDynamically(collection) {
 
         //update title and text and image
         newcard.querySelector(".card-title").innerHTML = title;
-        newcard.querySelector(".card-length").innerHTML = hikeLength + "km";
+        // newcard.querySelector(".card-length").innerHTML = hikeLength + "km";
+        newcard.querySelector(".card-length").innerHTML =
+          "Length: " +
+          doc.data().length +
+          " km <br>" +
+          "Duration: " +
+          doc.data().hike_time +
+          "min <br>" +
+          "Last updated: " +
+          doc.data().last_updated.toDate().toLocaleDateString();
         newcard.querySelector(".card-text").innerHTML = details;
         newcard.querySelector(".card-image").src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
         newcard.querySelector("a").href = "eachHike.html?docID=" + docID; //link to each hike page
+        newcard.querySelector("i").id = "save-" + docID; //guaranteed to be unique
+        newcard.querySelector("i").onclick = () => saveBookmark(docID); // for backend purposes
 
         //Optional: give unique ids to all elements for future use
         // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
@@ -150,5 +169,3 @@ function displayCardsDynamically(collection) {
       });
     });
 }
-
-displayCardsDynamically("hikes"); //input param is the name of the collection
